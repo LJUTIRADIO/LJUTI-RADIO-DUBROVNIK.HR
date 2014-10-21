da5518a50f6d2accd4b01716e96a72a026934207
LJUTIRADIO <mojradijo2011@gmail.com>
2014-10-21 21:49:19 +0200

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,5 @@
-console.log('This would be the main JS file.');
+<script type="text/javascript">var cstrpuid = 226993;var cstrpwidth = "500";var cstrpheight = "325";</script>
+<script type="text/javascript" src="http://cdn.caster.fm/0070B7/widgets/player.js"></script>
+<!--   DO NOT REMOVE THE LINKS BELOW, THEY  WILL BE HIDDEN (AND WILL HELP US A LOT)   -->
+<a id="cstrplb" href="http://www.caster.fm/">Free Shoutcast Hosting</a><a id="cstrplb2" href="http://www.caster.fm/">Radio Stream Hosting</a>
+<div id="cstrpdiv"></div>
