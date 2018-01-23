3906a9eecb805acc1c1c381ea36c1c45de2fac96
LJUTIRADIO <mojradijo2011@gmail.com>
2018-01-23 20:20:17 +0100

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,1 @@
-//myradiostream.com/embed/free.php?s=LJUTIRADIO&btnstyle=circle-o&stats=true
+//myradiostream.com/embed/free.php?s=LJUTIRADIO&btnstyle=circle-o&stats=true<iframe src="//myradiostream.com/embed/free.php?s=LJUTIRADIO&btnstyle=circle-o&stats=true" name="MD2ZB2f9JGY4" frameborder="0" scrolling="no" style="width:800px;height:150px;border:0;margin:0;overflow:hidden;border-radius:30px;"></iframe>
